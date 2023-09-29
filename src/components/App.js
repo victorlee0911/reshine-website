205422253f6a62be0e05e958d97717e460227312
victorlee0911 <44325672+victorlee0911@users.noreply.github.com>
2023-09-29 14:46:29 -0700

    working version of site without pics in react

---
+++ src/components/App.js
@@ -0,0 +1,92 @@
+import React from 'react'
+
+function App (){
+    return (
+        <div class="layout">
+            <div class="nav-bar">
+                <a href="./index.html">
+                    <img class="logo" src="./src/assets/logo.png" alt="WeShine Logo" />
+                </a>
+                <ul class="links">
+                    <li><a href="./about.html">About</a></li>
+                    <li><a href="./contact.html">Contact</a></li>
+                </ul>
+            </div>
+
+            <div class="cover-section">
+                <div class="cover-text">
+                    <h1>ReShine Renewables</h1>
+                </div>
+                <img class="cover-photo" src="./src/assets/Cover Picture.jpg" alt="microgrid renewable energy" />
+            </div>
+
+            <div class="grid-container dark-blue">
+                <div class="grid">
+                    <div class="grid-item">
+                        <img src="./src/assets/BESS (Acentech).jpg" alt="alt" />
+                    </div>
+                    <div class="grid-item text-container">
+                        <div class="text-section">
+                            <h2>Testing 1</h2>
+                            <p>ipiscididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                            <button class="learn-more">Learn More</button>
+                        </div>
+                    </div>
+                    <div class="grid-item text-container">
+                        <div class="text-section">
+                            <h2>Testing 2</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                            <button class="learn-more">Learn More</button>
+                        </div>
+                    </div>
+                    <div class="grid-item">
+                        <img src="./src/assets/AeroFarms-Vertical-Farm-212-Rome-Interior-1-1280x720.png" alt="test" />
+                    </div>
+                    <div class="grid-item">
+                        <img src="./src/assets/energies-15-04062-g001 Microgrid.png" alt="test" />
+                    </div>
+                    <div class="grid-item text-container">
+                        <div class="text-section">
+                            <h2>Microgrid with BMS</h2>
+                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+                            <button class="learn-more">Learn More</button>
+                        </div>
+                    </div>
+
+                </div>
+            </div>
+
+            <div class="section">
+                <div class="section-title">
+                    <h2>Products</h2>
+                </div>
+                <div class="section-flex">
+                    <div class="section-item">
+                        <a href="#"><img src="./src/assets/energies-15-04062-g001 Microgrid.png" alt="test" /></a>
+                    </div>
+                    <div class="section-item">
+                        <img src="./src/assets/energies-15-04062-g001 Microgrid.png" alt="test" />
+                    </div>
+                </div>
+            </div>
+            <div class="section">
+                <div class="section-title">
+                    <h2>Market</h2>
+                </div>
+                <div class="section-flex">
+                    <div class="section-item">
+                        <img src="./src/assets/energies-15-04062-g001 Microgrid.png" alt="test" />
+                    </div>
+                    <div class="section-item">
+                        <img src="./src/assets/energies-15-04062-g001 Microgrid.png" alt="test" />
+                    </div>
+                </div>
+            </div>
+
+            <footer>footer</footer>
+        </div>
+    )
+};
+
+
+export default App;
